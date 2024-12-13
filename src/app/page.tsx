89d668ca49e3e1300a0d69be9ad022acaa019b2b
Bikash <tulsiojhaamg@gmail.com>
2024-12-13 14:09:07 +0530

    added git link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ import TabView from "@/components/compounds/tab-view";
 import ComponentSection from "@/components/molecules/component-section";
 import FormSection from "@/components/molecules/form-section";
 import MobileDrawer from "@/components/molecules/mobile-drawer";
+import { Button } from "@/components/ui/button";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { ILayout } from "@/utils/types";
-import { Sidebar } from "lucide-react";
+import { LucideGithub, Sidebar } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 
 const Tabs = ({ layouts }: { layouts: ILayout[] }) => {
@@ -41,20 +43,29 @@ export default function Home() {
       className="grid grid-rows-[50px,auto] max-h-screen min-h-screen overflow-hidden"
       style={{ maxWidth: "100vw" }}
     >
-      <div className="h-full border-b border-black/10 items-center justify-between px-4 flex flex-row sticky top-0 z-50 bg-white">
+      <div className="h-[50px] border-b border-black/10 items-center justify-between px-4 flex flex-row sticky top-0 z-50 bg-white">
         <div className="flex-1 text-sm">Dynamic form builder</div>
-        <div className="md:hidden">
-          <MobileDrawer
-            trigger={
-              <button className="hover:bg-black/10 rounded-lg p-2">
-                <Sidebar size={22} />
-              </button>
-            }
-          >
-            <div className="pt-2">
-              <Tabs layouts={layouts} />
-            </div>
-          </MobileDrawer>
+        <div className="flex flex-row items-center gap-1">
+          <div>
+            <Button variant={"ghost"} asChild>
+              <Link href={"https://github.com/tulsiojha/form-builder-react"}>
+                <LucideGithub size={22} />
+              </Link>
+            </Button>
+          </div>
+          <div className="md:hidden">
+            <MobileDrawer
+              trigger={
+                <Button variant={"ghost"}>
+                  <Sidebar size={22} />
+                </Button>
+              }
+            >
+              <div className="pt-2">
+                <Tabs layouts={layouts} />
+              </div>
+            </MobileDrawer>
+          </div>
         </div>
       </div>
       <div className="flex flex-col md:flex-row max-h-[calc(100vh-50px)] max-w-screen">
