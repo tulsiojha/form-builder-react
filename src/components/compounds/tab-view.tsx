51305c2a992a9cc7f4e09a01a8ab823cb0e3a63f
Bikash <tulsiojhaamg@gmail.com>
2024-12-13 15:41:53 +0530

    fixed tab alignments

--- src/components/compounds/tab-view.tsx
+++ src/components/compounds/tab-view.tsx
@@ -9,7 +9,7 @@ import { ITabs } from "@/utils/types";
 const TabView = ({ triggers = [], contents = [], defaultValue }: ITabs) => {
   return (
     <NativeTabs defaultValue={defaultValue} className="h-full">
-      <TabsList className="grid w-full grid-cols-3 max-w-[300px] items-center justify-center m-auto">
+      <TabsList className="grid w-full grid-cols-3 max-w-[300px] items-center justify-center md:m-auto">
         {triggers.map((t) => (
           <TabsTrigger key={t.value} value={t.value}>
             {t.label}
